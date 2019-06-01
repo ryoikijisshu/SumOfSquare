a7565bf6b40fc2200802e02f7911f5b681e35382
Yu-shun <dle04755@kwansei.ac.jp>
2019-06-02 00:40:35 +0900

    平方和関数バージョン！！

--- app/src/main/java/kgu/klab/sumofsquare/MainActivity.kt
+++ app/src/main/java/kgu/klab/sumofsquare/MainActivity.kt
@@ -1,5 +1,6 @@
 package kgu.klab.sumofsquare
 
+import android.hardware.camera2.params.OisSample
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -8,31 +9,43 @@ import java.lang.Math.pow
 class MainActivity : AppCompatActivity() {
 
     val list_a = listOf(3,4,4,5,5,5,5,6,6,7)
-    //val list_b = listOf(1,2,4,4,5,5,5,6,8,10)
+    val list_b = listOf(1,2,4,4,5,5,5,6,8,10)
 
-    var sum_a = 0
-    var mean_a = 0.0
     var res_a = 0
+    var res_b = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        for (i in list_a.indices){
-            sum_a = sum_a + list_a[i]
-        }
-        println("合計:"+sum_a)
-
-        mean_a = sum_a.toDouble() / (list_a.lastIndex+1)
-
-        println("平均:"+mean_a+"\n配列長:"+list_a.lastIndex)
-
-        for (i in list_a.indices) {
-            res_a = res_a + pow(list_a[i] - mean_a, 2.0).toInt()
+        button_a.setOnClickListener {
+            res_a = sumofsquare(list_a)
+            a.text = list_a.toString()+"\n\n標本aの平方和:"+res_a
         }
 
-        a.text = "標本aの平方和:"+res_a
-        println("平方和:"+res_a)
+        button_b.setOnClickListener {
+            res_b = sumofsquare(list_b)
+            a.text = list_b.toString()+"\n\n標本bの平方和:"+res_b
+        }
+
+    }
+
+    fun sumofsquare(sample:List<Int>): Int {
+        var sum = 0
+        var mean = 0.0
+        var kekka = 0
+
+        for (i in sample.indices){
+            sum = sum + sample[i]
+        }
+
+        mean = sum.toDouble() / (sample.size)
+
+        for (i in sample.indices) {
+            kekka = kekka + pow(sample[i] - mean, 2.0).toInt()
+        }
+
+        return kekka
     }
 
 }
