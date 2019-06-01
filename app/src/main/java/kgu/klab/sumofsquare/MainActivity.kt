d9275c0a85b7917d07f55a64f0d059244c70dc9e
ryochin0114 <ryochin@kwansei.ac.jp>
2019-06-01 11:59:14 +0900

    関数を使用して平方和を求めました

--- app/src/main/java/kgu/klab/sumofsquare/MainActivity.kt
+++ app/src/main/java/kgu/klab/sumofsquare/MainActivity.kt
@@ -13,45 +13,24 @@ class MainActivity : AppCompatActivity() {
         var arrayofDoubleA: Array<Double> = arrayOf(3.0, 4.0, 4.0, 5.0, 5.0, 5.0, 5.0, 6.0, 6.0, 7.0)
         //小標本B
         var arrayofDoubleB: Array<Double> = arrayOf(1.0, 2.0, 4.0, 4.0, 5.0, 5.0, 5.0, 6.0, 8.0, 10.0)
-        var a: Int = 0
-        var b: Int = 0
-        var d:Int = 0
-        var e:Int = 0
-        var sum: Double = 0.0
+
+        val SSA:Double = functionSS(arrayofDoubleA)
+        val SSB:Double = functionSS(arrayofDoubleB)
+        TextView.text = SSA.toString()
+        TextView2.text = SSB.toString()
+
+
+    }
+    fun functionSS( arrayDouble:Array<Double>): Double {
         var sum2: Double = 0.0
-        var sum3: Double = 0.0
-        var sum4:Double = 0.0
-        var sum5:Double = 0.0
-        var sum6:Double =0.0
-        var result: Double = 0.0
-        var result2:Double = 0.0
-        var n: Double = 0.0
-        var m:Double  =  0.0
-        //Aの平方和を求める
-        for (a in arrayofDoubleA) {
-            sum += a*a
+        var sum: Double = 0.0
+        var SS: Double = 0.0
+        for (a in arrayDouble) {
+            sum2 += a * a
+            sum += a
         }
-        for( b in arrayofDoubleA) {
-            sum2 += b
-            n+=1
-        }
-        sum3 = ( sum2*sum2 ) / n
-
-        result = sum-sum3
-        TextView.text = result.toString()
-        //Bの平方和を求める
-        for (d in arrayofDoubleB) {
-            sum4 += d*d
-        }
-        for( e in arrayofDoubleB) {
-            sum5 += e
-            m+=1
-        }
-        sum6 = ( sum5*sum5 ) / m
-
-        result2 = sum4-sum6
-        TextView2.text = result2.toString()
-
+        SS = sum2 - (sum * sum) / arrayDouble.size
+        return SS
     }
 
 }
