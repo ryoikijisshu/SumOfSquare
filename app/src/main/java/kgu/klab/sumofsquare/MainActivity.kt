14318a2d59a6abf76b1521fb13ee557155f58f7a
Yaekamishima <evm29500@kwansei.ac.jp>
2019-06-01 12:04:54 +0900

    ゼミの課題終わりました

--- app/src/main/java/kgu/klab/sumofsquare/MainActivity.kt
+++ app/src/main/java/kgu/klab/sumofsquare/MainActivity.kt
@@ -7,36 +7,28 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        val numA = intArrayOf(3,4,4,5,5,5,5,6,6,7)
-        val numB = intArrayOf(1,2,4,4,5,5,5,6,8,10)
-        var SumA = 0
-        var SumB = 0
-        var SumC = 0
-        var SumD = 0
-        var ResultA = 0
-        var ResultB = 0
-
-        for (i in 0..9){
-            SumA += numA[i] * numA[i]
-            SumC += numB[i] * numB[i]
-        }
-        for (i in 0..9){
-            SumB += numA[i]
-            SumD += numB[i]
-            if(i ==9){
-                SumB *= SumB /10
-                SumD *= SumD /10
-            }
-        }
-
-        ResultA = SumA - SumB
-        ResultB = SumC - SumD
-
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        resultA.text = ResultA.toString()
-        resultB.text = ResultB.toString()
+        val numA = intArrayOf(3, 4, 4, 5, 5, 5, 5, 6, 6, 7)
+        val numB = intArrayOf(1, 2, 4, 4, 5, 5, 5, 6, 8, 10)
+
+        resultA.text = "Aの平方和は"+SumOfSquare(numA).toString()
+        resultB.text = "Bの平方和は"+SumOfSquare(numB).toString()
+
     }
+
+    fun SumOfSquare(num: IntArray):Int{
+        var sos = 0
+        var ss2 = 0
+        var sum  = 0
+
+        for (i in num) {
+            ss2 += i*i
+            sum += i
+        }
+        sos =  ss2 - sum * sum / num.size
+        return(sos)
+    }
+
 }
