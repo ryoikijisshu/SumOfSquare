1058e286cf687464fb9edaa85f8e6e33eee8a7a8
okap0910 <fhl25209@kwansei.ac.jp>
2019-06-05 11:59:59 +0900

    平方和の課題（関数内で計算する）が終わりました

--- app/src/main/java/kgu/klab/sumofsquare/MainActivity.kt
+++ app/src/main/java/kgu/klab/sumofsquare/MainActivity.kt
@@ -11,21 +11,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val A = arrayOf(3,4,4,5,5,5,5,6,6,7)
         val B = arrayOf(1,2,4,4,5,5,5,6,8,10)
-        var sumA:Double = 0.0
-        var sumB:Double = 0.0
-        var sumA2:Double = 0.0
-        var sumB2:Double = 0.0
-        var SSA:Double = 0.0
-        var SSB:Double = 0.0
-        for (i in 0..9){
-            sumA += A[i]
-            sumB += B[i]
-            sumA2 += A[i]*A[i]
-            sumB2 += B[i]*B[i]
+        answerA.text ="標本Aの平方和は" + SS(A)
+        answerB.text ="標本Bの平方和は" + SS(B)
+
+    }
+    fun SS(array: Array<Int>): Double{
+        var sum:Double = 0.0
+        var sum2:Double = 0.0
+        for (i in 0..array.size-1){
+            sum += array[i]
+            sum2 += array[i]*array[i]
         }
-        SSA = sumA2-sumA*sumA/10
-        SSB = sumB2-sumB*sumB/10
-        answerA.text ="標本Aの平方和は" + SSA
-        answerB.text ="標本Bの平方和は" + SSB
+        return sum2-(sum*sum)/array.size
     }
 }
